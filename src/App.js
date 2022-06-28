1bc0799bb109aebca009103bea75c1e5ad3f2436
Awwie <acecace@live.com>
2022-06-28 02:02:49 -0500

    Done? I think?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
+//useEffect will run our side effect function every time the component re-renders.render -> useEffect -> setState -> re-render -> useEffect
+
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
+
 
 function App() {
+  
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+
+   //reset the count variable back to 0 after 5 sec
+   useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
+
+  // useEffect(() => {
+  //   //side effect fn
+  //   console.log("App useEffect (Count) called");
+  // }, [count]); //run when count variable has changed
+
+  //updates the chrome tab name based on the user input field
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text]);
 
-  console.log("Component rendering");
+  console.log("App component rendering");
 
-  return <button>Click Me</button>;
+  function handleClick() {
+    setCount((count) => count + 1)
+  }
+
+  function handleText(e) {
+    setText(e.target.value)
+  }
+
+  
+
+  return (
+    <div>
+      <button onClick={handleClick} >I've been clicked {count} times</button>
+      <input 
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={handleText}
+      />
+      <DogPics />
+
+
+    </div>
+    
+  )
 }
 
 export default App;
--- src/DogPics.js
+++ src/DogPics.js
@@ -1,22 +1,27 @@
+//without the 2nd argu of [], we'd run into an infinite loop:
+//render -> useEffect -> setImages -> render -> useEffect -> setImages -> render -> etc...
+
 import React, { useState, useEffect } from "react";
 
-// WARNING: this useEffect will run in an infinite loop!
-// to fix, pass an empty array as the second argument for useEffect
 function DogPics() {
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState([]); //initial render of empty obj
 
+  //If we write a component that updates state from inside the useEffect callback, we'll see an issue without using the [] as 2nd argu.
+  //infinite loop can happen without it
   useEffect(() => {
-    console.log("useEffect");
+    console.log("DogPic useEffect called");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
-        setImages(data.message);
+        console.log("DogPic Fetch setState");
+        console.log(data.message)
+        setImages(data.message); // this means setter Fn has set "images" to be the new obj
       });
-  });
+  }, []); // empty brackets mean this will run on first render only
 
-  console.log("render");
+  console.log("DocPic component rendering");
 
+  //images being the new obj needs to be mapped to render the array of images to the page
   return (
     <div>
       {images.map((image) => (
